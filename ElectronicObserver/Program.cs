77920fbc210f61dd7fd29ca417e0a8f5000e07a7
CNA-Bld <cna.bld@gmail.com>
2018-03-06 22:43:08 +0800

    Workaround .Net 4.5 default TLS version mismatch
    net40 does not need this workaround patch

--- ElectronicObserver/Program.cs
+++ ElectronicObserver/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,6 +19,8 @@ namespace ElectronicObserver
 		[STAThread]
 		static void Main(string[] args)
 		{
+
+			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
 			Application.ThreadException += Application_ThreadException;
 
